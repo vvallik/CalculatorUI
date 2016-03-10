4a6e5efb47697007d3dd96fef7237384a3f6004d
Vaiko Vällik <vaikovallik@gmail.com>
2016-03-10 16:51:54 +0200

    change of ui

--- app/src/main/java/com/example/vaiko/calculatorui/MainActivity.java
+++ app/src/main/java/com/example/vaiko/calculatorui/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+   /*     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+*/
 
     }
 
